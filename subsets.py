624359a978a1a769cc5e5045459dd15453580061
gamagegit <cgamage92@yahoo.com>
2022-12-04 17:39:24 -0800

    finalized solution

--- subsets.py
+++ subsets.py
@@ -4,4 +4,32 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        output = [[]]
+        def back_track(nums,temp,start,sub_lenght):
+            
+            if len(temp)==sub_lenght:
+                temp_copy = temp[:]
+                output.append(temp_copy)
+                return
+            
+            for i in range(start,len(nums)):
+                temp.append(nums[i])
+                back_track(nums,temp,i+1,sub_lenght)
+                temp.pop()
         
+        for j in range(1,len(nums)+1): 
+            back_track(nums,[],0,j)
+        
+        
+        return output
+            
+
+
+nums = [0]           
+obj = Solution()
+
+print(obj.subsets(nums))
+            
+            
+            
+            
